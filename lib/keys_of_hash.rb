c8e26ca2c102e5b3b47ce69c348f3bff62e3ac2c
Xabier Casan <xabier_abia@hotmail.com>
2019-03-31 20:51:28 +0000

    Automatically backed up by Learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,6 +1,10 @@
 class Hash
+#  def keys_of(*arguments)
+#    self.collect {|k, e| k if arguments.include?(e)}.keep_if {|e| e}
+#  end
   def keys_of(*arguments)
     array = []
+    #use self since each closes the scope. 
     self.each do |key, value|
       if arguments.include?(value)
         array << key 
@@ -8,4 +12,5 @@ class Hash
     end
     array
   end
-end
+end```
+
